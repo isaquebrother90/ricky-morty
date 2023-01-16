5b09c1ecbb3105ca700a70f17ec5dcdf6306c9e5
isaquebrother90 <improgramcode@gmail.com>
2023-01-16 10:10:35 -0300

    Documentando api com Swagger

--- backend/src/main/java/com/personagens/rickymorty/controller/RickyMortyController.java
+++ backend/src/main/java/com/personagens/rickymorty/controller/RickyMortyController.java
@@ -2,6 +2,10 @@ package com.personagens.rickymorty.controller;
 
 import com.personagens.rickymorty.dto.external.CharacterClientResponseDTO;
 import com.personagens.rickymorty.service.impl.CharacterServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(value = "Rick and Morty API")
 @RestController
 @RequestMapping("/api/v1/characters")
 public class RickyMortyController {
@@ -19,11 +24,13 @@ public class RickyMortyController {
         this.characterServiceImpl = characterServiceImpl;
     }
 
+    @ApiOperation(value = "Get all characters")
     @GetMapping
     public ResponseEntity<List<CharacterClientResponseDTO>> listAll() {
         return ResponseEntity.ok(characterServiceImpl.listAll());
     }
 
+    @ApiOperation(value = "Get characters by name")
     @GetMapping("/{name}")
     public ResponseEntity<CharacterClientResponseDTO> searchByName(@PathVariable("name") String name) {
         return ResponseEntity.ok(characterServiceImpl.searchByName(name));
